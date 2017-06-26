4395935165217240d3a67a00c477b6acdf25008a
Malcom <gtshilombowanticale@adneom.com>
2017-06-26 14:53:04 +0200

    try to use profile provider in service

--- Poc_Keyboard/app/src/main/java/adneom/poc_keyboard/service/SimpleIME.kt
+++ Poc_Keyboard/app/src/main/java/adneom/poc_keyboard/service/SimpleIME.kt
@@ -1,6 +1,7 @@
 package adneom.poc_keyboard.service
 
 import adneom.poc_keyboard.R
+import android.content.ClipDescription
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.inputmethodservice.InputMethodService
@@ -15,6 +16,20 @@ import android.support.v13.view.inputmethod.EditorInfoCompat
 import android.support.v4.content.FileProvider
 import android.util.Log
 import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.support.v13.view.inputmethod.InputConnectionCompat
+import android.os.Build
+import android.R.attr.mimeType
+import android.R.attr.description
+import android.support.v13.view.inputmethod.InputContentInfoCompat
+
+
+
+
 
 
 /**
@@ -29,7 +44,21 @@ class SimpleIME : InputMethodService(), KeyboardView.OnKeyboardActionListener {
 
     var cap : Boolean = false
 
-    //crate our Keaybaord View
+    val nameFile : String = "image.png"
+
+    var fileOutput : File? = null
+
+    var editorInfo : EditorInfo? = null
+
+    var myMimes : Array<String>? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        createFile()
+    }
+
+    //create our Keaybaord View
     override fun onCreateInputView(): View {
         kv = layoutInflater.inflate(R.layout.layout_keybaord, null) as KeyboardView;
         keyboard = Keyboard(this,R.xml.qwerty)
@@ -39,7 +68,8 @@ class SimpleIME : InputMethodService(), KeyboardView.OnKeyboardActionListener {
     }
 
     override fun onStartInputView(info: EditorInfo?, restarting: Boolean) {
-        supportedContentMimeTypes(info)
+        editorInfo = info
+        supportedContentMimeTypes(editorInfo)
     }
 
     override fun swipeRight() {}
@@ -66,7 +96,42 @@ class SimpleIME : InputMethodService(), KeyboardView.OnKeyboardActionListener {
             }
             Keyboard.KEYCODE_DONE -> {
                 //ic.sendKeyEvent(KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER))
-                //val contentUri : Uri = FileProvider.getUriForFile(this,this.applicationContext.packageName)
+                val contentUri : Uri = FileProvider.getUriForFile(this,this.applicationContext.packageName,fileOutput)
+
+                // As you as an IME author are most likely to have to implement your own content provider
+                // to support CommitContent API, it is important to have a clear spec about what
+                // applications are going to be allowed to access the content that your are going to share.
+                val flag: Int
+                if (Build.VERSION.SDK_INT >= 25) {
+                    // On API 25 and later devices, as an analogy of Intent.FLAG_GRANT_READ_URI_PERMISSION,
+                    // you can specify InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION to give
+                    // a temporary read access to the recipient application without exporting your content
+                    // provider.
+                    flag = InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION
+                } else {
+                    // On API 24 and prior devices, we cannot rely on
+                    // InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION. You as an IME author
+                    // need to decide what access control is needed (or not needed) for content URIs that
+                    // you are going to expose. This sample uses Context.grantUriPermission(), but you can
+                    // implement your own mechanism that satisfies your own requirements.
+                    flag = 0
+                    try {
+                        // TODO: Use revokeUriPermission to revoke as needed.
+                        grantUriPermission(
+                                editorInfo!!.packageName, contentUri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    } catch (e: Exception) {
+                        Log.e(TAG, "grantUriPermission failed packageName=" + editorInfo!!.packageName
+                                + " contentUri=" + contentUri, e)
+                    }
+
+                }
+
+                val inputContentInfoCompat = InputContentInfoCompat(
+                        contentUri,
+                        ClipDescription("test image", arrayOf<String>("image/png")),
+                        null /* linkUrl */)
+
+                InputConnectionCompat.commitContent(getCurrentInputConnection(), getCurrentInputEditorInfo(), inputContentInfoCompat, flag, null)
             }
             else -> {
                 var code : Char = primaryCode.toChar()
@@ -100,7 +165,20 @@ class SimpleIME : InputMethodService(), KeyboardView.OnKeyboardActionListener {
     }
 
     private fun createFile(){
-       // var drawable : Drawable = this.getDrawable(R.drawable.dess)
+        fileOutput = File (filesDir,nameFile)
+        var resourceReader : InputStream =  this.resources.openRawResource(R.drawable.dessert_android)
+        var dataWriter : OutputStream = FileOutputStream(fileOutput)
+
+        var buf : ByteArray = ByteArray(1024)
+        var len : Int = resourceReader.read(buf)
+        while ( len > 0) {
+            dataWriter.write(buf,0,len)
+            len = resourceReader.read(buf)
+            Log.i("Adneom","in")
+        }
+        dataWriter.flush()
+        dataWriter.close()
+        Log.i("Adneom", fileOutput!!.name.plus(fileOutput!!.path))
     }
 
 }
